82dbf21fdc44414b9f42f4bf1b45beea8edcd1a3
Ebrar <16155052@mersin.edu.tr>
2022-08-17 15:09:48 +0300

    v2

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ const express = require('express');
 
 const app = express();
 
+//template engine
+app.set("view engine","ejs");
+
+
+//middleware
+app.use(express.static('public'));
+
+//routes
 app.get('/', (req, res) => {
-  res.status(200).send('index sayfası');
+  res.status(200).render('index',{
+    page_name:"index"
+  });
+
 });
 
+app.get('/about', (req, res) => {
+    res.status(200).render('about',{
+        page_name:"about"
+    });
+  });
 const port = 5000;
 app.listen(port, () => {
   console.log(`Uygulama ${port} portunda çalışıyor.`);

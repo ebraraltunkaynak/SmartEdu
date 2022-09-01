eb96557743f85684b08aa370bf674e8104aba6d1
Ebrar <16155052@mersin.edu.tr>
2022-09-01 14:59:28 +0300

    update course

--- controllers/courseControllers.js
+++ controllers/courseControllers.js
@@ -114,9 +114,25 @@ exports.releaseCourse = async (req, res) => {
 
 exports.deleteCourse = async (req, res) => {
   try {
-  const course=   await Course.findOneAndRemove({slug:req.params.slug})
+    const course = await Course.findOneAndRemove({ slug: req.params.slug });
     req.flash('success', `${course.name} has been deleted successfully`);
 
+    res.status(200).redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'failed',
+      error,
+    });
+  }
+};
+
+exports.updateCourse = async (req, res) => {
+  try {
+    const course = await Course.findOne({ slug: req.params.slug });
+    course.name = req.body.name;
+    course.description = req.body.description;
+    course.category = req.body.category;
+    course.save();
     res.status(200).redirect('/users/dashboard');
   } catch (error) {
     res.status(400).json({

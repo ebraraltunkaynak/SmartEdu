b791f32ace95fdda3dcd409249af2272feab43e7
Ebrar <16155052@mersin.edu.tr>
2022-09-03 14:29:48 +0300

    category details

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -5,5 +5,6 @@ const categoryController = require('../controllers/categoryControllers');
 const router = express.Router();
 
 router.route('/').post(categoryController.createCategory); //localhost/categories
+router.route('/:id').delete(categoryController.deleteCategory); //delete category
 
 module.exports = router;
